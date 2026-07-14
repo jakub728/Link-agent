1043e8ba7c8560db59abf63d89a5f4b4001ecada
Jakub Sulisz <sulisz.jakub@gmail.com>
2026-07-14 11:43:47 +0200

    update

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -66,7 +66,7 @@ router.get(
         return res.status(404).json({ message: "Brak użytkownika w bazie" });
       }
 
-      return res.status(200).json({ message: user });
+      return res.status(200).json( user.login );
     } catch (error) {
       console.error(error);
       next(error as any);
