4d2f147ecb97ad11ccf933bf47c6fdee7317baf1
Jakub Sulisz <sulisz.jakub@gmail.com>
2026-07-22 12:51:58 +0200

    update

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,10 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import SatkurierAI from "../assets/Satkurier.png";
 
 export default function Layout() {
   return (
     <>
-      <h1>SI Satkurier generator</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <h1>Satkurier </h1>
+        <img
+          src={SatkurierAI}
+          style={{ backgroundColor: "white", height: "36px", width: "36px" }}
+        />
+      </div>
       <header>
         <Navbar />
       </header>
